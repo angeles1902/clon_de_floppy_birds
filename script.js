5db6c225e0f8acaf56414874ba602e079c68299a
Your Name <you@example.com>
2025-02-26 12:47:31 -0500

    agragamos el array de background y propiedades

--- script.js
+++ script.js
@@ -24,4 +24,18 @@ function preload() {
 
 function create() {
     const background = []
+
+    for (let i = 0; i < 100; i++) {
+        background.push(this.add.sprite(0, config.height, 'background')
+        .setOrigin(1, 1)
+        .setScale(1.1)
+    )
+        
+    }
+
+    Phaser.Actions.AlignTo(background, Phaser.Display.Align.RIGHT_BOTTOM)
+
+    this.bird = this.physics.add.image(config.width / 2, config.height / 2, "bird")
+        .setColliderWorldBounds(true)
+        .setScale(.05)
 }
