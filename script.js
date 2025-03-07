c616d6388045c917f41444a2cad944a1375439cf
Your Name <you@example.com>
2025-03-07 12:45:50 -0500

    se copio el codigo

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const config = {
 new Phaser.Game(config)
 
 function preload() {
-    this.load.image("bird","/bird.png")
-    this.load.image("background", "/background.jpg")
-    this.load.image("pipe", "pipe.png")
+    this.load.image("bird","./bird.png")
+    this.load.image("background", "./background.jpg")
+    this.load.image("pipe", "./pipe.png")
 }
 
 function create() {
@@ -38,4 +38,39 @@ function create() {
     this.bird = this.physics.add.image(config.width / 2, config.height / 2, "bird")
         .setColliderWorldBounds(true)
         .setScale(.05)
+    
+    for (let i = 1; i < 100; i++) {
+        this.pipeDown = this.physics.add.staticImage(1000*(i/2), config.height, 'pipe')
+        .setOrigin(1,1)
+        .setScale(.5)
+        .refreshBody()
+
+        this.pipeUp =this.physics.add.staticImage(1000*(i/2), 0, 'pipe')
+        .setOrigin(1, 1)
+        .setScale(.5)
+        .setAngle(180)
+        .refreshBody()
+        
+        this.hitBird = false
+
+        this.pipeUp.body.setOffset(-this.pipeUp.width / 2, -this.pipeUp.height / 2)
+
+        this.physics.add.collider(this.pipeUp, this.bird, handleHit, null, this)
+        this.physics.add.collider(this.pipeDown, this.bird)
+    }
+
+    this.cursors = this.inpu.keyboard.createCursorKeys()
+    this.keys = this.input.keyboard.addKeys("W, A, S, D")
+    this.cameras.main.startFollow(this.bird)
+    this.cameras.main.setBounds(0, 0, 5000, config.height)
+    this.physics.world.setBounds(0, 0, 5000, config.height)
+    this.bird.setVelocityX(120)
+}
+
+function update () {
+    if (this.hitBird) {return}
+    this.bird.rotation = this.bird.body.angle
+    if (this.cursors.up.isDown) {
+        this.bird.setVelocityY(-250)
+    }
 }
